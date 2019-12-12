7ed1d0c76c0b535b6922b094798e298baa0c20a4
João Pedro Sá Medeiros <jpsmedeiros@id.uff.br>
2019-12-11 23:47:47 -0300

    Update compile.sh

--- projeto-escrita/compile.sh
+++ projeto-escrita/compile.sh
@@ -1,1 +1,11 @@
-mpic++ Decimate.cpp Isosurface.cpp Gyroid.cpp math3D.cpp LinkedList.cpp main.cpp -o main.o && mpirun --hostfile hostfile -np $1 main.o $2 $3 $4
+#PBS -S  /bin/bash
+#PBS -N MARCHING-TETRAHEDRA
+#PBS -q  linux.q
+#PBS -l nodes=8:ppn=2
+
+cd $PBS_O_WORKDIR
+
+# OPENMPI 1.10 + GCC 5.3
+source /hpc/modulos/bash/openmpi-1.10-gcc53.sh
+mpic++ Decimate.cpp Isosurface.cpp Gyroid.cpp math3D.cpp LinkedList.cpp main.cpp -o main.o
+mpiexec -np ${PBS_NP} -hostfile  ${PBS_NODEFILE} $1 main.o $2 $3 $4
